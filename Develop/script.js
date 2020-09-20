068b0fd6fec74a62b5a00c1a1e9c1e9588a79f42
jess-smith49 <js.jessicasmith774@gmail.com>
2020-09-20 11:32:11 -0500

    fixed error for generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,11 @@
 
 
-//create an array for characters, lowercase, upper case, and numbers or an object array?
+//Made individual arrays, call random function, use .length
 
-//var specialChar = [" !","#","$","%","&","()","*","+",",","-",".","/",":",";","<","=",">","?","@","[/]","^","_","`","{","|","}","~"];
-//var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-//for (i = 0, i > 8 || i < 128; i ++){
-
-
-
-
-
-
-
+   var specialChar = ["!","#","$","%","&","()","*","+",",","-",".","/",":",";","<","=",">","?","@","[/]","^","_","`","{","|","}","~"];
+   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+   var randomNum = ["1", "2", "3", "4", "4", "6", "7", "8", "9", "0"];
 
 
 // Get references to the #generate element
@@ -22,11 +13,56 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword(password) {  
-  //calling the select password function
-  select();
+  
+    window.alert("Choose Password Criteria");
+  
+    //password lenght criteria
+    var num = window.prompt("Select a password between 8 and 128 characters");
+    if (num === "" || num > 128 || num < 8){
+      window.alert("Must select from range");
+      return null;
+    }
+   
+  
+    
+  //chooses criteria for password in prompt
+    var pass = window.prompt("Choose from Uppercase, lowercase, or special characters");
+    var specialCharBool = false;
+    var upperCaseBool = false;
+    var lowerCaseBool = false;
+  
+  
+    pass = pass.toLowerCase();
+    if (pass.includes("uppercase")){
+      upperCaseBool = true;  
+    }
+    if (pass.includes("lowercase")){
+      lowerCaseBool = true;  
+    }
+    if (pass.includes("special characters")){
+      specialCharBool = true;  
+    }
+    
 
+    //gets random # from arrays based on num
+    var finalPass = "";
+    var currentChar 
+    for (i = 0; i < num; i ++){
+      finalPass += specialChar[Math.floor(Math.random() * specialChar.length)]
+      finalPass += upperCase[Math.floor(Math.random () * upperCase.length)]
+      finalPass += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+      finalPass += randomNum[Math.floor(Math.random() * randomNum.length)]
+      
+    }
 
-  var password = generatePassword();
+    //displays finalPass
+    console.log(finalPass);
+  
+  
+
+    var password = generatePassword(password);
+
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -36,28 +72,13 @@ function writePassword(password) {
 }
 
 // Add event listener to generate button
-//When this is clicked, the write password functionis called
+//When this is clicked, the write password function is called
 generateBtn.addEventListener("click", writePassword);
 
 
-var select = function(){
-  window.alert("Choose Password Criteria");
 
-  var num = window.prompt("Select a password between 8 and 128 characters");
-  if (num === "" || num > 128 || num < 8){
-    window.alert("Must select from range");
-  }
-  else{
-    choose();
-  }
-
-}
-
-var choose = function (){
-  var pass = window.prompt("Choose from Uppercase, lowercase, or special characters");
   
 
-}
 
 
 
