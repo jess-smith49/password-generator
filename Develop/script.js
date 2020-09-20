a78296985bf27c128fedac06bb747aeb602d8865
jess-smith49 <js.jessicasmith774@gmail.com>
2020-09-20 12:56:25 -0500

    working code

--- Develop/script.js
+++ Develop/script.js
@@ -14,60 +14,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword(password) {  
   
-    window.alert("Choose Password Criteria");
-  
-    //password lenght criteria
-    var num = window.prompt("Select a password between 8 and 128 characters");
-    if (num === "" || num > 128 || num < 8){
-      window.alert("Must select from range");
-      return null;
-    }
-   
-  
-    
-  //chooses criteria for password in prompt
-    var pass = window.prompt("Choose from Uppercase, lowercase, or special characters");
-    var specialCharBool = false;
-    var upperCaseBool = false;
-    var lowerCaseBool = false;
-  
-  
-    pass = pass.toLowerCase();
-    if (pass.includes("uppercase")){
-      upperCaseBool = true;  
-    }
-    if (pass.includes("lowercase")){
-      lowerCaseBool = true;  
-    }
-    if (pass.includes("special characters")){
-      specialCharBool = true;  
-    }
-    
+  var password = generatePassword();
 
-    //gets random # from arrays based on num
-    var finalPass = "";
-    var currentChar 
-    for (i = 0; i < num; i ++){
-      finalPass += specialChar[Math.floor(Math.random() * specialChar.length)]
-      finalPass += upperCase[Math.floor(Math.random () * upperCase.length)]
-      finalPass += lowerCase[Math.floor(Math.random() * lowerCase.length)]
-      finalPass += randomNum[Math.floor(Math.random() * randomNum.length)]
-      
-    }
-
-    //displays finalPass
-    console.log(finalPass);
-  
-  
-
-    var password = generatePassword(password);
-
-  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  console.log(password);
 
 }
 
@@ -76,7 +27,58 @@ function writePassword(password) {
 generateBtn.addEventListener("click", writePassword);
 
 
+//generate password function return value
+function generatePassword() {
 
+  window.alert("Choose Password Criteria");
+  
+  //password lenght criteria
+  var num = window.prompt("Select a password between 8 and 128 characters");
+  if (num === "" || num > 128 || num < 8){
+    window.alert("Must select from range");
+    return null;
+  }
+ 
+
+  
+//chooses criteria for password in prompt
+  var pass = window.prompt("Choose from Uppercase, lowercase, or special characters");
+  var specialCharBool = false;
+  var upperCaseBool = false;
+  var lowerCaseBool = false;
+
+
+ pass = pass.toLowerCase();
+  if (pass.includes("uppercase")){
+    upperCaseBool = true;  
+  }
+  if (pass.includes("lowercase")){
+    lowerCaseBool = true;  
+  }
+  if (pass.includes("special characters")){
+    specialCharBool = true;  
+  } 
+  
+  
+ 
+  //gets random # from arrays based on num
+  var finalPass = "";
+  //var currentChar 
+
+  for (i = 0; i < num; i ++){
+    finalPass += specialChar[Math.floor(Math.random() * specialChar.length)]
+    finalPass += upperCase[Math.floor(Math.random () * upperCase.length)]
+    finalPass += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    finalPass += randomNum[Math.floor(Math.random() * randomNum.length)]
+    
+  }
+
+  //displays finalPass*/
+  
+  console.log(finalPass);
+
+  
+}
   
 
 
